6aec865bda59edb555f8644df098c66625e381d3
Ruslan Mikhno <ruslan@mikhno.org>
2017-02-16 00:15:17 +0000

    Added the main plugin file with metadata

---
+++ bulk-change-media-author.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Bulk Change Media Author
+Description: Allows you to bulk change the author of media items
+Plugin Author: Ruslan Mikhno
+Version: 0.0.1
+Author URI: http://www.mikhno.org
+*/
+
+// TODO
